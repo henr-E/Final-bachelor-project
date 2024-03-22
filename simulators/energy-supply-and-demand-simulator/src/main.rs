f03ab19bb1fca630c9f3c08d17e80a594e2160d1
Said Yandarbiev <said.yandarbiev@student.uantwerpen.be>
2024-03-22 15:55:12 +0000

    !62: Feat/58/simulate effects of temperature
    Added simulator that simulates the change in demand of consumernodes based on the temperature.

--- simulators/energy-supply-and-demand-simulator/src/main.rs
+++ simulators/energy-supply-and-demand-simulator/src/main.rs
@@ -1,4 +1,6 @@
 use component_library::energy::{EnergyConsumerNode, EnergyProducerNode};
+use component_library::global::chrono::{NaiveDateTime, Timelike};
+use component_library::global::{TemperatureComponent, TimeComponent};
 use rand::prelude::*;
 use simulator_communication::{ComponentsInfo, Graph, Server, Simulator};
 use std::{env, net::SocketAddr, process::ExitCode, time::Duration};
@@ -42,6 +44,8 @@ impl Simulator for EnergySupplyAndDemandSimulator {
         ComponentsInfo::new()
             .add_required_component::<EnergyConsumerNode>()
             .add_required_component::<EnergyProducerNode>()
+            .add_optional_component::<TimeComponent>()
+            .add_optional_component::<TemperatureComponent>()
             .add_output_component::<EnergyConsumerNode>()
             .add_output_component::<EnergyProducerNode>()
     }
@@ -54,11 +58,32 @@ impl Simulator for EnergySupplyAndDemandSimulator {
     }
 
     fn do_timestep(&mut self, mut graph: Graph) -> Graph {
+        let current_temp = match graph.get_global_component_mut::<TemperatureComponent>() {
+            Some(temperature) => temperature.current_temp,
+            None => 17.5,
+        };
+
+        let mut binding = TimeComponent(NaiveDateTime::default());
+        let current_time = match graph.get_global_component_mut::<TimeComponent>() {
+            Some(timecomponent) => timecomponent,
+            None => &mut binding,
+        };
+        let time_effect = if current_time.0.hour() >= 6 && current_time.0.hour() <= 18 {
+            1.0
+        } else {
+            0.5
+        };
+        let temp_effect = (current_temp - 17.5).abs();
+        let target_demand = 100.0 + temp_effect * 10.0 * time_effect;
+
         for (_, _, component) in graph.get_all_nodes_mut::<EnergyConsumerNode>().unwrap() {
             let current_demand = component.demand;
+            let adjustment = rand::thread_rng().gen_range(-0.01..0.05);
             let delta_demand =
-                rand::thread_rng().gen_range(-25.0..25.0) * self.delta_time.as_secs() as f64;
-            component.demand = (current_demand + delta_demand).clamp(0.0, 500.0)
+                (target_demand - current_demand) * adjustment * self.delta_time.as_secs() as f64;
+            let mut rng = rand::thread_rng();
+            let direction_factor = if rng.gen_bool(0.1) { -1.0 } else { 1.0 };
+            component.demand = (current_demand + delta_demand * direction_factor).clamp(0.0, 500.0)
         }
 
         for (_, _, component) in graph.get_all_nodes_mut::<EnergyProducerNode>().unwrap() {
